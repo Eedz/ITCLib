2fbbbdca4f48af910123eadf68969c44f96499d1
e3bauer <e3bauer@uwaterloo.ca>
2019-01-11 15:08:21 -0500

    Added Data Access Folder Added Tests

---
+++ ITCLibTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ITCLibTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ITCLibTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4bd09f5d-073e-4aac-bf63-206a67881d55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ITCLibTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ITCSurveyReportLib;
+
+namespace ISISLibTest
+{
+    [TestClass]
+    public class DBActionsTest
+    {
+        [TestMethod]
+        public void TestVarNameChangesByID()
+        {
+            VarNameChange vc = DBAction.GetVarNameChangeByID(1);
+
+            Assert.IsNotNull(vc);
+
+            Assert.AreEqual(vc.ID, 1);
+            Assert.AreEqual(vc.OldName.VarName, "BQ11404");
+            Assert.AreEqual(vc.NewName.VarName, "BQ11160");
+            
+
+        }
+    }
+}
